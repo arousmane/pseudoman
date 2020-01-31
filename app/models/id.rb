a8196c5063db47276becbf0ee546ef0ae766a79c
Rahmane OUSMANE <ousmanedev@gmail.com>
2020-01-31 19:06:18 +0100

    Generate and Store ID if provided ID is already taken

--- app/models/id.rb
+++ app/models/id.rb
@@ -1,4 +1,13 @@
 class Id < ApplicationRecord
   validates :value, presence: true, uniqueness: true
   validates_format_of :value, :with => /\A[A-Z]{3}\z/
+
+
+  def self.lock(id)
+    $redis.setex(id, 300, 'locked')
+  end
+
+  def self.locked?(id)
+    !$redis.get(id).nil?
+  end
 end
